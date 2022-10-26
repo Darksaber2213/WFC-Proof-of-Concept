caba79d9713ce5026374ac8dc9d9d1bcc3cffc7d
Darksaber2213 <darksaber2213@outlook.com>
2022-10-25 19:32:27 -0600

    Start work on tile and tileset structure.

--- classes/tile.cs
+++ classes/tile.cs
@@ -1,3 +1,14 @@
-function Tile::constructor(%this)
+// A "Tile" is a data structure that contains information about a possible generative state of a Node in the Grid.
+// The Tile contains keys that represent constraints on which possible Tiles it can neighbour.
+// Each Tile also contains the bricks to build when the Grid has finished collapsing. TODO: Implement.
+
+function Tile::constructor(%this, %constraintN, %constraintE, %constraintS, %constraintW, %constraintU, %constraintD)
 {
+	// Constraint keys. Usually represented as a single uppercause A-Z character.
+	%this.constraintN = %constraintN;
+	%this.constraintE = %constraintE;
+	%this.constraintS = %constraintS;
+	%this.constraintW = %constraintW;
+	%this.constraintU = %constraintU;
+	%this.constraintD = %constraintD;
 }
--- classes/tileset.cs
+++ classes/tileset.cs
@@ -1,3 +1,50 @@
-function Tileset::constructor(%this)
+// A "Tileset" is a set of Tiles that the Grid uses to determine constraints to collapse the Nodes into definitive states.
+
+function Tileset::constructor(%this, %tilesetName)
 {
+	%this.tileCount = 0;
+	%this.loadTilesFromTilesetName(%tilesetName);
+}
+
+function Tileset::loadTilesFromTilesetName(%this, %tilesetName)
+{
+	%directory = $WFC::Root @ "/tilesets/" @ %tilesetName;
+	%this.loadTilesFromDirectory(%directory);
+}
+
+function Tileset::loadTilesFromDirectory(%this, %directory)
+{
+	%reader = new FileObject();
+	%pattern = %directory @ "/*.txt";
+	echo("Loading tiles from directory pattern:\c1" SPC %pattern);
+	
+	for(%file = findFirstFile(%pattern); %file !$= ""; %file = findNextFile(%pattern))
+	{
+		echo("Loading tile from:\c1" SPC %file);
+		%tileData = "";
+		%reader.openForRead(%file);
+		
+		while(!%reader.isEOF())
+		{
+			%line = %reader.readLine();
+			%tileData = %tileData NL %line;
+		}
+		
+		%reader.close();
+		%tileData = trim(%tileData);
+		echo("\c0Read tile data:\c1" SPC fileName(%file) @ "\c0, contents:\c4" SPC %tileData);
+		
+		%this.tile[%this.tileCount++ - 1] = construct
+		(
+			Tile,
+			getField(%tileData, 0),
+			getField(%tileData, 1),
+			getField(%tileData, 2),
+			getField(%tileData, 3),
+			getField(%tileData, 4),
+			getField(%tileData, 5)
+		);
+	}
+	
+	%reader.delete();
 }
