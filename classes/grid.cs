f202f735e70c52148d1dbd12f787979191e44e9f
Darksaber2213 <darksaber2213@outlook.com>
2022-10-25 19:32:47 -0600

    Work on some minor documentation.

--- classes/grid.cs
+++ classes/grid.cs
@@ -1,3 +1,5 @@
+// A "Grid" is a container for Nodes, and its size represents the total size of the generative area.
+
 function Grid::constructor(%this)
 {
 }
--- classes/node.cs
+++ classes/node.cs
@@ -1,3 +1,6 @@
+// A "Node" is a superposition of Tiles in the Grid.
+// Nodes are collapsed into definitive Tiles based on their surroundings.
+
 function Node::constructor(%this)
 {
 }
